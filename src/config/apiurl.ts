ea63d546dfc2cc77ab81db12d15f3c17b5b54688
namminimi <tbvhdaos@naver.com>
2023-04-14 20:10:38 +0900

    project
    서버주소 수정

--- src/config/apiurl.ts
+++ src/config/apiurl.ts
@@ -1,2 +1,3 @@
 
-export const API_URL = "http://localhost:8004"
+/* export const API_URL = "http://localhost:8004" */
+export const API_URL = "https://port-0-self-introduction-server-1maxx2alggg3mf5.sel3.cloudtype.app"
